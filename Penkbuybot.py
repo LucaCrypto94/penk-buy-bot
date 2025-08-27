cc56de3f6f56429e1c7ebb69ddf4149343368b73
LucaCrypto94 <azzarelli.jobs@gmail.com>
2025-08-27 12:50:10 +0200

    Update Penkbuybot.py

--- Penkbuybot.py
+++ Penkbuybot.py
@@ -11,13 +11,13 @@ from collections import deque
 # ─────────────────────────────
 # Configuration (via Env Vars)
 # ─────────────────────────────
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-THREAD_MESSAGE_ID = os.getenv("THREAD_MESSAGE_ID", None)
+TELEGRAM_TOKEN = os.getenv("8438467171:AAGtRIvbecoG4EzE01nlK2jNVWwazcRbvrU")
+CHAT_ID = os.getenv("-2843689356")
+THREAD_MESSAGE_ID = os.getenv("THREAD_MESSAGE_ID", "1")
 
-POOL_ADDRESS = os.getenv("POOL_ADDRESS", "").lower()
-TOKEN_ADDRESS = os.getenv("TOKEN_ADDRESS", "").lower()
-TOKEN_SYMBOL = os.getenv("TOKEN_SYMBOL", "TOKEN")
+POOL_ADDRESS = os.getenv("POOL_ADDRESS", "0x71942200c579319c89c357b55a9d5c0e0ad2403e").lower()
+TOKEN_ADDRESS = os.getenv("TOKEN_ADDRESS", "0x82144c93bd531e46f31033fe22d1055af17a514c").lower()
+TOKEN_SYMBOL = os.getenv("TOKEN_SYMBOL", "PENK")
 MIN_TOKEN_AMOUNT = int(os.getenv("MIN_TOKEN_AMOUNT", 1))
 CIRCULATING_SUPPLY = int(os.getenv("CIRCULATING_SUPPLY", 1_000_000_000))
 
@@ -246,3 +246,4 @@ if __name__ == "__main__":
         print("❌ Missing environment variables. Please check TELEGRAM_TOKEN, CHAT_ID, POOL_ADDRESS, TOKEN_ADDRESS.")
     else:
         asyncio.run(monitor_gecko_trades())
+
